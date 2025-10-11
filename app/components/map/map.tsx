02751dc8bcf38c64a54f97c7af16ee5940ccb0a6
Tim Vahlbrock <tim@vahlbrock.de>
2025-10-12 00:16:53 +0200

    show existing bus routes

--- app/components/map/map.tsx
+++ app/components/map/map.tsx
@@ -1,17 +1,37 @@
 'use client';
 
-import {MapContainer, Marker, Polyline, TileLayer} from "react-leaflet";
+import {MapContainer, Polyline, TileLayer} from "react-leaflet";
 import 'leaflet/dist/leaflet.css';
 import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css';
 import 'leaflet-defaulticon-compatibility';
-import {DraggableLine} from "@/app/components/map/DraggableLine";
-import {LatLng} from "leaflet";
+import {FeatureCollection} from "geojson";
+import {useEffect, useState} from "react";
 
 export interface MapProps {
     isAdding: boolean;
 }
 
+function useBusData() {
+    const [busData, setBusData] = useState<FeatureCollection | null>(null);
+
+    useEffect(() => {
+        fetch('/data/busses-borken.geojson')
+            .then(response => response.json())
+            .then(data => setBusData(data))
+            .catch(error => console.error('Error fetching bus data:', error));
+    }, []);
+
+    return busData;
+}
+
 export default function Map(props: MapProps) {
+    const busData = useBusData();
+
+    const filteredFeatures = (busData?.features ?? [])
+        .filter(feature => feature.properties)
+        .filter(feature => feature.properties!.type === "route")
+        .filter(feature => feature.properties!.ref?.startsWith("C"));
+
     return (
         <MapContainer
             center={[51.83692, 6.61892]}
@@ -24,20 +44,38 @@ export default function Map(props: MapProps) {
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
 
-            <DraggableLine
-                initialMarkers={
-                    [
-                        new LatLng(51.82, 6.60),
-                        new LatLng(51.83, 6.60),
-                        new LatLng(51.85, 6.60),
-                        new LatLng(51.83, 6.61),
-                        new LatLng(51.825, 6.61),
-                        new LatLng(51.825, 6.60), // the marker on the intersection
-                        new LatLng(51.825, 6.58)
-                    ]
+            {/*<DraggableLine*/}
+            {/*    initialMarkers={*/}
+            {/*        [*/}
+            {/*            new LatLng(51.82, 6.60),*/}
+            {/*            new LatLng(51.83, 6.60),*/}
+            {/*            new LatLng(51.85, 6.60),*/}
+            {/*            new LatLng(51.83, 6.61),*/}
+            {/*            new LatLng(51.825, 6.61),*/}
+            {/*            new LatLng(51.825, 6.60), // the marker on the intersection*/}
+            {/*            new LatLng(51.825, 6.58)*/}
+            {/*        ]*/}
+            {/*    }*/}
+            {/*    isAdding={props.isAdding}*/}
+            {/*    />*/}
+            {filteredFeatures
+                .map(feature => {
+                if(feature.geometry.type == "LineString") {
+                    const coordinates = feature.geometry.coordinates.map(coord => [coord[1], coord[0]] as [number, number]);
+                    return <Polyline
+                        key={feature.properties?.id}
+                        positions={coordinates}
+                        pathOptions={{ color: 'black', weight: 3 }} />;
+                } else if(feature.geometry.type == "MultiLineString") {
+                    return feature.geometry.coordinates.map((lineCoords, index) => {
+                        const coordinates = lineCoords.map(coord => [coord[1], coord[0]] as [number, number]);
+                        return <Polyline
+                                    key={`${feature.properties?.id}-${index}`}
+                                    positions={coordinates}
+                                    pathOptions={{ color: 'black', weight: 3 }} />;
+                    });
                 }
-                isAdding={props.isAdding}
-                />
+            })}
         </MapContainer>
     )
 }
