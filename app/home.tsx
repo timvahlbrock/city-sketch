c1d680bb9941157ca1858c35c4b76608b8060016
Tim Vahlbrock <tim@vahlbrock.de>
2025-11-07 14:56:46 +0100

    remove unncessary dynamic import

--- app/home.tsx
+++ app/home.tsx
@@ -3,17 +3,13 @@
 import { useEffect, useRef, useState } from "react";
 import { Button, Card, Radio, Splitter } from "antd";
 import { Map as LeafletMap } from "leaflet";
-import dynamic from "next/dynamic";
 import { Layer } from "@/app/layers";
 import { addSection } from "@/app/addSection";
+import MapComponent from "@/app/components/map/map";
 
 export interface HomeProps {
   layers: Layer[];
 }
-
-const DynamicMap = dynamic(() => import("./components/map/map"), {
-  ssr: false,
-});
 
 export default function Home({ layers }: HomeProps) {
   const [orientation, setOrientation] = useState<"horizontal" | "vertical">(
@@ -43,9 +39,9 @@ export default function Home({ layers }: HomeProps) {
         onResize={() => mapRef.current?.invalidateSize({ pan: true })}
       >
         <Splitter.Panel defaultSize={"70%"}>
-          <DynamicMap ref={mapRef} isAdding={false}>
+          <MapComponent ref={mapRef} isAdding={false}>
             {activeLayer?.element}
-          </DynamicMap>
+          </MapComponent>
           ;
         </Splitter.Panel>
         <Splitter.Panel className={"bg-white"}>
