1f4bff46bcd5484afa28fe28315d175899c80cbc
Tim Vahlbrock <tim@vahlbrock.de>
2025-12-13 13:35:16 +0100

    chore: re-enable browsers

--- playwright.config.ts
+++ playwright.config.ts
@@ -39,29 +39,29 @@ export default defineConfig({
       name: "chromium",
       use: { ...devices["Desktop Chrome"] },
     },
-    // ...(process.env.CI
-    //   ? [
-    //       {
-    //         name: "firefox",
-    //         use: { ...devices["Desktop Firefox"] },
-    //       },
-    //
-    //       {
-    //         name: "webkit",
-    //         use: { ...devices["Desktop Safari"] },
-    //       },
-    //
-    //       /* Test against mobile viewports. */
-    //       {
-    //         name: "Mobile Chrome",
-    //         use: { ...devices["Pixel 5"] },
-    //       },
-    //       {
-    //         name: "Mobile Safari",
-    //         use: { ...devices["iPhone 12"] },
-    //       },
-    //     ]
-    //   : []),
+    ...(process.env.CI
+      ? [
+          {
+            name: "firefox",
+            use: { ...devices["Desktop Firefox"] },
+          },
+
+          {
+            name: "webkit",
+            use: { ...devices["Desktop Safari"] },
+          },
+
+          /* Test against mobile viewports. */
+          {
+            name: "Mobile Chrome",
+            use: { ...devices["Pixel 5"] },
+          },
+          {
+            name: "Mobile Safari",
+            use: { ...devices["iPhone 12"] },
+          },
+        ]
+      : []),
   ],
 
   /* Run your local dev server before starting the tests */
