77ae30ef53df1a503ad6a1e43cc8a9ba4654abb5
Tim Vahlbrock <tim@vahlbrock.de>
2025-12-13 13:02:48 +0100

    Launch convex before starting tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -65,9 +65,16 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  webServer: {
-    command: "npm run start",
-    url: "http://localhost:3000",
-    reuseExistingServer: !process.env.CI,
-  },
+  webServer: [
+    {
+      command: "npm run start",
+      url: "http://localhost:3000",
+      reuseExistingServer: !process.env.CI,
+    },
+    {
+      command: "npm run convex",
+      url: "http://localhost:3210",
+      reuseExistingServer: !process.env.CI,
+    },
+  ],
 });
