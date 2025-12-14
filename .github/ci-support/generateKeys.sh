47ed396a0346789c8f6218fee522fe609738bc45
Tim Vahlbrock <tim@vahlbrock.de>
2025-12-14 12:47:26 +0100

    chore: ci: fail generateKeys script on error

--- .github/ci-support/generateKeys.sh
+++ .github/ci-support/generateKeys.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # from https://github.com/get-convex/convex-backend/issues/128
+set -e
 output=$(node ./github/ci-support/generateKeys.mjs)
 
 JWT_PRIVATE_KEY=$(echo "$output" | sed -n 's/^JWT_PRIVATE_KEY="\([^"]*\)"/\1/p')
@@ -13,7 +14,9 @@ ENV_CMD="npx convex env set"
 # auth will fail if these are not set
 if ! $ENV_CMD -- JWT_PRIVATE_KEY "$JWT_PRIVATE_KEY" >/dev/null; then
   echo "Failed to set JWT_PRIVATE_KEY"
+  exit 1;
 fi
 if ! $ENV_CMD -- JWKS "$JWKS" >/dev/null; then
   echo "Failed to set JWKS"
+  exit 1;
 fi
