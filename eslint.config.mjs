a1d16afb31f2be371368b53639ae4ef428064b35
Tim Vahlbrock <tim@vahlbrock.de>
2025-12-16 19:31:56 +0100

    chore: add convex eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,12 @@
 import nextPlugin from "@next/eslint-plugin-next";
 import prettierConfig from "eslint-config-prettier";
 import tsPlugin from "eslint-config-next/typescript";
+import convexPlugin from "@convex-dev/eslint-plugin";
 
 const eslintConfig = [
   nextPlugin.configs["core-web-vitals"],
   ...tsPlugin,
+  ...convexPlugin.configs.recommended,
   prettierConfig,
 ];
 
